40cd825d8e7c8a9beb5484a8b2cf63e1bfef358c
CarlosRa10 <carloseduardoramirez10@gmail.com>
2024-05-17 18:21:10 -0400

    detalles

--- Proyecto_Uno.py
+++ Proyecto_Uno.py
@@ -1,2 +1,3 @@
+#Poyecto de creacion de marca personal
 print('La cerveza se llama\n"' + input("Animal Favorito: ") + ' ' + input("De donde eres: ")+'"')
 #print("El nombre de tu cerveza\nes '" + input("Que ciudad te gustaria visitar?: ") + " " + input("Cual es tu color favorito?: ") + "'\nFelicitaciones!")
