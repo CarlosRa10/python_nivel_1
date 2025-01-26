20d06cd0f3fc22457da64bee7f59d0b28ad5998f
carlosra10 <carloseduardoramirez10@gmail.com>
2025-01-25 22:12:17 -0400

    Agregando comentarios de copilot

--- String.py
+++ String.py
@@ -1,6 +1,6 @@
 print('Hola '+"Carlos")
-print("Me llamo \"Carlos\"")# La barra invertida es un caracter especial que se usa para indicar que el siguiente caracter sera tomado como un caracter textual
-print("Esta es una linea \nY esta es otra linea")
+print("Me llamo \"Carlos\"")# La barra invertida(signo de escape) es un caracter especial que se usa para indicar que el siguiente caracter sera tomado como un caracter textual
+print("Esta es una linea \nY esta es otra linea")# \n es un caracter especial que se usa para indicar un salto de linea
 print("\tEsta es la tercera linea")#El tabulador es un espacio de 4 caracteres o sangria
 print("This isn\'t a number")
 print("Este signo \\ es una barra invertida")
