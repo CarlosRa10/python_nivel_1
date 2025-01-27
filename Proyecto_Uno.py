1896775b2856d9d5c19b0200cb6ab40bf5092935
carlosra10 <carloseduardoramirez10@gmail.com>
2025-01-26 23:41:56 -0400

    commit

--- Proyecto_Uno.py
+++ Proyecto_Uno.py
@@ -1,3 +1,3 @@
-#Poyecto de creacion de marca personal
+#Poyecto de creacion de marca personal que consiste en la creacion de una cerveza artesanal con un nombre unico y original
 print('La cerveza se llama\n"' + input("Animal Favorito: ") + ' ' + input("De donde eres: ")+'"')
 #print("El nombre de tu cerveza\nes '" + input("Que ciudad te gustaria visitar?: ") + " " + input("Cual es tu color favorito?: ") + "'\nFelicitaciones!")
